5d5df6dc6ab72064c1cb5a0283220993dd47dfbb
sobek1985 <sobek1985@gmail.com>
2015-08-07 13:55:03 +0100

    Init Commit

---
+++ MikeRobbinsSPEAKRichTextEditor/RichTextEditor.js
@@ -0,0 +1,40 @@
+﻿require.config({
+    paths: {
+        "Quill": "/sitecore/shell/client/MikeRobbins/Components/RichTextEditor/quill/dist/quill"
+    }
+});
+
+define(["sitecore", "jquery", "Quill"], function (Sitecore, jQuery, Quill) {
+
+    var model = Sitecore.Definitions.Models.ControlModel.extend({
+        initialize: function (options) {
+            this._super();
+
+            this.set("width", null);
+            this.set("height", null);
+
+        }
+ 
+    });
+
+
+    var view = Sitecore.Definitions.Views.ControlView.extend({
+        initialize: function (options) {
+            this._super();
+
+            this.model.set("width", this.$el.data("sc-width"));
+            this.model.set("height", this.$el.data("sc-height"));
+
+            var fullEditor = new Quill('#full-editor', {
+                theme: 'snow'
+            });
+
+            fullEditor.addModule('toolbar', {
+                container: '#toolbar'     // Selector for toolbar container
+            });
+
+        }
+    });
+
+    Sitecore.Factories.createComponent("RichTextEditor", model, view, ".sc-RichTextEditor");
+});
