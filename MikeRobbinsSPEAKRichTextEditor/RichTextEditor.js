1f5dddf655769f4ccaadeeaa5f87fb097a18e259
sobek1985 <sobek1985@gmail.com>
2015-10-07 13:14:47 +0100

    SPEAK 2.0 Javascript complete

--- MikeRobbinsSPEAKRichTextEditor/RichTextEditor.js
+++ MikeRobbinsSPEAKRichTextEditor/RichTextEditor.js
@@ -9,17 +9,10 @@
     Speak.component(["collection", "tinyMCE"], function (Collection, tinyMCE) {
         return Speak.extend({}, Collection.prototype, {
             initialized: function () {
+                this.on("change:text", this.UpdateRichText);
 
-                this.defineProperty("width", null);
-                this.defineProperty("height", null);
-                this.defineProperty("browserspellcheck", null);
-                this.defineProperty("resize", null);
-                this.defineProperty("plugins", null);
-                this.on("change:text", this.UpdateRichText, this);
-
-  
                 tinymce.init({
-                    selector: this.id,
+                    selector: "#" + this.id,
                     height: this.Height,
                     width: this.Width,
                     resize: this.Resize,
@@ -28,61 +21,19 @@
                     plugins: this.Plugins,
                     setup: function (ed) {
                         ed.on('change', function (e) {
-                            e.target.settings.speakContext.model.UpdateText(e.target.getContent());
+                            e.target.settings.speakContext.UpdateText(e.target.getContent());
                         });
                     }
                 });
             },
 
-            UpdateRichText: function (context) {
-                if (context.viewModel.text != null) {
-                    tinyMCE.get(context.id).setContent(context.viewModel.text());
-                }
+            UpdateRichText: function () {
+                tinymce.get(this.id).setContent(this.Text);
             },
 
             UpdateText: function (content) {
-                this.viewModel.text(content);
+                this.Text = content;
             }
         });
     }, "RichTextEditor");
 })(Sitecore.Speak);
-
-
-//    var view = Sitecore.Definitions.Views.ControlView.extend({
-//        initialize: function (options) {
-//            this._super();
-//            this.model.set("text", this.$el.val());
-//            this.model.set("width", this.$el.data("sc-width"));
-//            this.model.set("height", this.$el.data("sc-height"));
-//            this.model.set("resize", this.$el.data("sc-resize"));
-//            this.model.set("id", this.$el.data("sc-id"));
-//            this.model.set("plugins", this.$el.data("sc-plugins"));
-
-//            var spell = this.$el.data("sc-browser-spellcheck") == "0";
-
-//            this.model.set("browserspellcheck", spell);
-
-//            var id = "#" + this.model.viewModel.id();
-
-//            tinyMCE.init({
-//                selector: id,
-//                height: this.model.viewModel.height(),
-//                width: this.model.viewModel.width(),
-//                resize: this.model.viewModel.resize(),
-//                browser_spellcheck: this.model.viewModel.browserspellcheck(),
-//                speakContext: this,
-//                plugins: this.model.viewModel.plugins(),
-//                setup: function (ed) {
-//                    ed.on('change', function (e) {
-//                        e.target.settings.speakContext.model.UpdateText(e.target.getContent());
-//                    });
-//                }
-//            });
-
-//        }
-
-
-//    });
-
-//    Sitecore.Factories.createComponent("RichTextEditor", model, view, ".sc-RichTextEditor");
-//});
