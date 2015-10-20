658cdd976bbb15108192c9275912a639a7ed77f2
sobek1985 <sobek1985@gmail.com>
2015-10-20 14:00:06 +0100

    Remove unused usings

--- MikeRobbinsSPEAKRichTextEditor/Models/RichTextEditorRenderingModel.cs
+++ MikeRobbinsSPEAKRichTextEditor/Models/RichTextEditorRenderingModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Sitecore.Speak.Components.Models;
+﻿using Sitecore.Speak.Components.Models;
 
 namespace MikeRobbinsSPEAKRichTextEditor.Models
 {
